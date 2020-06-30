b0f6ace7154bd7eb93991dea600e93c6d0740e98
Durgnan <durgnan45@gmail.com>
2020-06-30 21:12:50 +0530

    React Assignment 1

---
+++ react/confusion/src/components/DishdetailComponent.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component{
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            
+        }
+    }
+
+    renderDish(dish) {
+        if (dish != null) {
+            return (
+                <div>
+                    <div className='row'>
+                        <div className='col-12 col-md-5 m-1'>
+                            <Card>
+                                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                                <CardBody>
+                                    <CardTitle>{dish.name}</CardTitle>
+                                    <CardText>{dish.description}</CardText>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        <div className='col-12 col-md-5 m-1'>
+                            <h4>Comments</h4>
+                            {this.renderComments(dish.comments)}
+                        </div>
+                    </div>
+                </div>
+
+            );
+        } else {
+            return (
+                <div></div>
+            );
+        }
+    }
+
+    renderComments(comments) {
+        if (comments != null) {
+            const com = comments.map((comment) => {
+                return (
+                    <div key={comment.id}>
+                        <CardText>
+                            <li className="mt-3">{comment.comment}</li>
+                            <li className="mt-3">{`-- ${comment.author} , ${comment.date}`}</li>
+                        </CardText>
+                    </div>
+                )
+            })
+            return (
+                <ul className="list-unstyled">
+                    {com}
+                </ul>
+            )
+
+        } else {
+            return (<div></div>)
+        }
+    }
+
+    render() {
+
+
+        return (
+            <div>
+                <div className="row">
+                    {this.renderDish(this.props.dish)}
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default DishDetail;
--- react/confusion/src/components/MenuComponent.js
+++ react/confusion/src/components/MenuComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle} from 'reactstrap';
+import DishDetail from './DishdetailComponent'
+
 
 class Menu extends Component{
     constructor(props){
@@ -15,25 +17,7 @@ class Menu extends Component{
         this.setState({selectedDish:dish});
     }
 
-    renderDish(dish){
-        if(dish != null){
-            return (
-              <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name} ></CardImg>
-                <CardBody>
-                  <CardTitle>{dish.name}</CardTitle>
-                <CardText>{dish.description}</CardText>
-                </CardBody>
-              </Card>
-            );
-        }
-        else{
-            return(
-                <div></div>
-            )
-        }
-    }
-
+    
     render(){
         const menu = this.props.dishes.map((dish) =>{
             return (
@@ -55,9 +39,8 @@ class Menu extends Component{
                         {menu}
                     
                 </div>
-                <div className="row">
-                    {this.renderDish(this.state.selectedDish)}
-                </div>
+                <DishDetail dish={this.state.selectedDish} />
+                
             </div>
         );
     }
