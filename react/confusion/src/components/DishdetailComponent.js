0e0515b05568d83b4825154d29b9a3eac53e5f79
Durgnan <durgnan45@gmail.com>
2020-07-02 01:12:49 +0530

    Removed Unused Imports

--- react/confusion/src/components/DishdetailComponent.js
+++ react/confusion/src/components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class DishDetail extends Component{
 
--- react/confusion/src/components/MenuComponent.js
+++ react/confusion/src/components/MenuComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Card,CardImg,CardImgOverlay,CardText,CardBody,CardTitle} from 'reactstrap';
+import {Card,CardImg,CardImgOverlay,CardTitle} from 'reactstrap';
 
 
 class Menu extends Component{
