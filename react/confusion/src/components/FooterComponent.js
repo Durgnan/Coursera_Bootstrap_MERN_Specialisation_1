d3023f5cde7dc0e32548517bedc7cf642bd8c1a9
Durgnan <durgnan45@gmail.com>
2020-07-02 23:27:51 +0530

    React Router

--- react/confusion/src/components/FooterComponent.js
+++ react/confusion/src/components/FooterComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from 'react-router-dom'
 
 
 function Footer(props) {
@@ -10,16 +11,16 @@ function Footer(props) {
               <h5>Links</h5>
               <ul className="list-unstyled">
                 <li>
-                  <a href="/">Home</a>
+                  <Link to="/home">Home</Link>
                 </li>
                 <li>
-                  <a href="/">About</a>
+                  <Link to="/aboutus">About</Link>
                 </li>
                 <li>
-                  <a href="/">Menu</a>
+                  <Link to="/menu">Menu</Link>
                 </li>
                 <li>
-                  <a href="contactus.html">Contact</a>
+                  <Link to="/contactus">Contact</Link>
                 </li>
               </ul>
             </div>
@@ -85,7 +86,7 @@ function Footer(props) {
           </div>
         </div>
       </div>
-    )
+    );
 }
 
 
--- react/confusion/src/components/HeaderComponent.js
+++ react/confusion/src/components/HeaderComponent.js
@@ -1,15 +1,65 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand, Jumbotron } from "reactstrap";
-
+import { Navbar, NavbarBrand, Jumbotron,Nav,NavbarToggler,Collapse,NavItem } from "reactstrap";
+import {NavLink} from 'react-router-dom';
 class Header extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isNavOpen : false
+        }
+        this.toggleNav = this.toggleNav.bind(this);
+    }
+
+
+    toggleNav() {
+        this.setState({isNavOpen: !this.state.isNavOpen})
+    }
+
+
+
+
   render() {
     return (
       <React.Fragment>
-        <Navbar dark>
+        <Navbar dark expand="md">
           <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            <NavbarToggler onClick={this.toggleNav} />
+            <NavbarBrand className="mr-auto" href="/">
+              <img
+                src="assets/images/logo.png"
+                height="30"
+                width="41"
+                alt="Ristorante Con Fusion"
+              />
+            </NavbarBrand>
+            <Collapse navbar isOpen={this.state.isNavOpen}>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink className="nav-link" to="/home">
+                    <span className="fa fa-home fa-lg"> Home</span>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/aboutus">
+                    <span className="fa fa-info fa-lg"> About Us</span>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/menu">
+                    <span className="fa fa-list fa-lg"> Menu</span>
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/contactus">
+                    <span className="fa fa-address fa-lg"> Contact Us</span>
+                  </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
           </div>
         </Navbar>
+
         <Jumbotron>
           <div className="container">
             <div className="row row-header">
