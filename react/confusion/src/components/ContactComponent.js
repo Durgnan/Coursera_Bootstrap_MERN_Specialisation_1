babad71e2d1488b7c3622644c24dfd1d56830cae
Durgnan <durgnan45@gmail.com>
2020-07-03 15:28:53 +0530

    Single Page Applications Part 2

--- react/confusion/src/components/ContactComponent.js
+++ react/confusion/src/components/ContactComponent.js
@@ -1,8 +1,22 @@
 import React from "react";
-
+import { Breadcrumb, BreadcrumbItem} from 'reactstrap'
+import { Link } from 'react-router-dom';
+ 
 function Contact(props) {
   return (
     <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/home">Home</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>Contact Us</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>Contact Us</h3>
+          <hr />
+        </div>
+      </div>
       <div className="row row-content">
         <div className="col-12">
           <h3>Location Information</h3>
@@ -36,7 +50,7 @@ function Contact(props) {
             >
               <i className="fa fa-phone"></i> Call
             </a>
-            <a role="button" className="btn btn-info">
+            <a role="button" className="btn btn-info" href="/">
               <i className="fa fa-skype"></i> Skype
             </a>
             <a
--- react/confusion/src/components/DishdetailComponent.js
+++ react/confusion/src/components/DishdetailComponent.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
-    function RenderDish({dish})  {
+    function RenderDish({dish,comments})  {
         if (dish != null) {
             return (
                 <div>
@@ -18,7 +19,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
                         </div>
                         <div className='col-12 col-md-5 m-1'>
                             <h4>Comments</h4>
-                            <RenderComments comments={dish.comments} />
+                            <RenderComments comments={comments} />
                         </div>
                     </div>
                 </div>
@@ -59,9 +60,19 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
         return (
           <div className="container">
             <div className="row">
-              <RenderDish
-                dish={props.dish}
-              />
+              <Breadcrumb>
+                <BreadcrumbItem>
+                  <Link to="/menu">Menu</Link>
+                </BreadcrumbItem>
+                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+              </Breadcrumb>
+              <div className="col-12">
+                <h3>{props.dish.name}</h3>
+                <hr />
+              </div>
+            </div>
+            <div className="row">
+              <RenderDish dish={props.dish} comments={props.comments} />
             </div>
           </div>
         );
