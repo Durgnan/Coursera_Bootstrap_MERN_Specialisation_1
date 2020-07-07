f0badfe8eceb45feb8390051348f4d511c5d7728
Durgnan <durgnan45@gmail.com>
2020-07-07 14:42:17 +0530

    React Redux Form Validation

--- react/confusion/src/components/ContactComponent.js
+++ react/confusion/src/components/ContactComponent.js
@@ -3,6 +3,12 @@ import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row} from 'reactstrap'
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors} from 'react-redux-form';
 
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => (val) && (val.length >= len);
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
+
 class Contact extends Component{
 
   constructor(props){
@@ -99,9 +105,23 @@ render() {
                   id="firstname"
                   name="firstname"
                   placeholder="First Name"
-                  
+                  validators={{
+                    required,
+                    minLength: minLength(3),
+                    maxLength: maxLength(15),
+                  }}
                 />
-                </Col>
+                <Errors
+                  className="text-danger"
+                  model=".firstname"
+                  show="touched"
+                  messages={{
+                    required: "Required",
+                    minLength: "Must be greater than 2 characters",
+                    maxLength: "Must be 15 characters or less",
+                  }}
+                />
+              </Col>
             </Row>
             <Row className="form-group">
               <Label md={2} htmlFor="lastname">
@@ -113,8 +133,22 @@ render() {
                   model=".lastname"
                   id="lastname"
                   name="lastname"
+                  validators={{
+                    required,
+                    minLength: minLength(3),
+                    maxLength: maxLength(15),
+                  }}
                   placeholder="Last Name"
-                  
+                />
+                <Errors
+                  className="text-danger"
+                  model=".lastname"
+                  show="touched"
+                  messages={{
+                    required: "Required",
+                    minLength: "Must be greater than 2 characters",
+                    maxLength: "Must be 15 characters or less",
+                  }}
                 />
               </Col>
             </Row>
@@ -129,7 +163,24 @@ render() {
                   id="telnum"
                   name="telnum"
                   placeholder="Tel. Number"
-                  />
+                  validators={{
+                    required,
+                    minLength: minLength(3),
+                    maxLength: maxLength(15),
+                    isNumber,
+                  }}
+                />
+                <Errors
+                  className="text-danger"
+                  model=".telnum"
+                  show="touched"
+                  messages={{
+                    required: "Required",
+                    minLength: "Must be greater than 2 numbers",
+                    maxLength: "Must be 15 numbers or less",
+                    isNumber: "Must be a number",
+                  }}
+                />
               </Col>
             </Row>
             <Row className="form-group">
@@ -143,7 +194,20 @@ render() {
                   id="email"
                   name="email"
                   placeholder="Email"
-                   />
+                  validators={{
+                    required,
+                    validEmail,
+                  }}
+                />
+                <Errors
+                  className="text-danger"
+                  model=".email"
+                  show="touched"
+                  messages={{
+                    required: "Required",
+                    validEmail: 'Invalid Email Address'
+                  }}
+                />
               </Col>
             </Row>
             <Row className="form-group">
@@ -164,7 +228,7 @@ render() {
                   model=".contactType"
                   className="form-control"
                   name="contactType"
-                      >
+                >
                   <option>Tel.</option>
                   <option>Email</option>
                 </Control.select>
@@ -181,7 +245,7 @@ render() {
                   name="message"
                   rows="12"
                   className="form-control"
-                 ></Control.textarea>
+                ></Control.textarea>
               </Col>
             </Row>
             <Row className="form-group">
