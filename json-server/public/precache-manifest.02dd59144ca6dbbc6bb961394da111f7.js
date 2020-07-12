ec902c4874eae58a9775a528a1feceafcb7f9a68
Durgnan <durgnan45@gmail.com>
2020-07-12 16:33:22 +0530

    React Course Deployed and completed

---
+++ json-server/public/precache-manifest.02dd59144ca6dbbc6bb961394da111f7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7862800b129c2b8a39971f8a66aece33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb56d6a477f5c994f615",
+    "url": "/static/css/2.1bd367ea.chunk.css"
+  },
+  {
+    "revision": "bbbe4831060139b4271b",
+    "url": "/static/css/main.782e60db.chunk.css"
+  },
+  {
+    "revision": "eb56d6a477f5c994f615",
+    "url": "/static/js/2.b0d443db.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b0d443db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbbe4831060139b4271b",
+    "url": "/static/js/main.a88b11e0.chunk.js"
+  },
+  {
+    "revision": "aff20233fc33123ab008",
+    "url": "/static/js/runtime-main.74f98a6f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
